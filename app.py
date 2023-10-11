0e9ca23432ceb3363d939dea54633191c39f3932
Prompt Engineer <prompt.engineer48@gmail.com>
2023-10-11 21:52:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from autogen import AssistantAgent, UserProxyAgent
+
+config_list = [
+    {
+        'model': 'gpt-4-0613',
+        'api_key': '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'
+    }
+]
+
+llm_config = {'config_list': config_list}
+
+#create an instance of AssistanAgent
+assistant = AssistantAgent(
+    name = "assistant",
+    llm_config=llm_config
+)
+
+#create an instance of UserProxyAgent
+user_proxy = UserProxyAgent(
+    name="user_proxy",
+    system_message='A Human input',
+    human_input_mode="NEVER"
+)
+
+user_proxy.initiate_chat(
+    assistant,
+    message = """Find the latest paper about gpt-4 on arxiv and find its potential application in software"""
+)
